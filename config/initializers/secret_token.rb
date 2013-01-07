fd933b807b8285d52a38e397cca0bf6f1bb0f989
ankit-playup <ankit@iplayup.com>
2013-01-07 11:00:15 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'd6b9d7efdfbf006aca82db8f57d80c833cccd07597019c140b84b1c8a0f5e76383cf72b1d71a8287a12b82490074477b878397983f4c4a43202f5aa24ae8e534'
